3582d28ca087ef81f9c67109e17e8c6449c1d2dc
Zelalem Belay <zelalembelay932@gmail.com>
2026-04-19 11:29:15 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
