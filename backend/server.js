a375e5c885dfe93906959945a7028d28e26ade9c
Zelalem Belay <zelalembelay932@gmail.com>
2026-04-21 16:25:59 +0300

    port change to 3000

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
